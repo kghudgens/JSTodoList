d8e284e78926253e67eaaad82cbb4379ee91f119
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-09 23:09:16 +0900

    logic to add inputted before footer

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 
 document.querySelector('.saveTask').addEventListener('click', function () {
     let task = document.querySelector('.task').value;
+    let newContent = document.createTextNode(task);
 
-    console.log(task);
+    const newDiv = document.createElement('div');
+    const currentDiv = document.getElementById('footer_div');
+    document.body.insertBefore(newDiv, currentDiv);
+    newDiv.appendChild(newContent);
+
+    console.log(newContent);
 });
