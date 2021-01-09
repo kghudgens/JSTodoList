df048b2d3a8d79acc32af9c9c3a8d3cab98ccd39
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-09 22:51:39 +0900

    event listener that takes in input

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-'use strict'
+'use strict';
 
+document.querySelector('.saveTask').addEventListener('click', function () {
+    let task = document.querySelector('.task').value;
 
+    console.log(task);
+});
